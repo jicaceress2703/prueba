71ed484afb7c8cf5772267371dc33e418114c674
jicaceress2703 <newphysicslife@gmail.com>
2022-04-25 21:50:31 -0500

    segunda versión PoW

--- PoW.py
+++ PoW.py
@@ -1,15 +1,20 @@
 from hashlib import sha256
 import random
 
-alfanum="abcdefghijklmnopqrstuvwxyz1234567890"
 comparador=False
-cadena="a"
+n=0
+cadena=": ¡Hola_mundo!"
 while(comparador==False):
-    cont=random.randint(0,len(alfanum)-1)
-    cadena=cadena+alfanum[cont]
-    hash_actual=sha256(cadena.encode()).hexdigest()
-    if(hash_actual.find("b00da")!=-1):
-        print(cadena)
-        print(hash_actual)
-        comparador=True
+    for i in range(33,127):
+        cadena_aux=chr(i)+cadena
+        hash_actual=sha256(cadena.encode()).hexdigest()
+        if(hash_actual.find("b00da")!=-1):
+            print(cadena)
+            print(hash_actual)
+            n=1
+        break
+    if(n==1):
+        comparador=True   
+    else:
+        cadena=chr(random.randint(33,126))+cadena
 
