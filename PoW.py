35fffdf85fcaaee48abbe06706fb19011386b20a
jicaceress2703 <newphysicslife@gmail.com>
2022-04-25 22:26:52 -0500

    comentarios

--- PoW.py
+++ PoW.py
@@ -5,9 +5,9 @@ comparador=False
 n=0
 cadena=": ¡Hello World!"
 while(comparador==False):
-    for i in range(33,127):
+    for i in range(33,127):#En este ciclo se generan los caracteres a partir de su codigo ASCII
         cadena_aux=chr(i)+cadena
-        hash_actual=sha256(cadena.encode()).hexdigest()
+        hash_actual=sha256(cadena.encode()).hexdigest()#se crea el hash en hexadecimal
         if(hash_actual.find("b00da")!=-1):
             print(cadena)
             print(hash_actual)
@@ -15,7 +15,7 @@ while(comparador==False):
             n=1
         break
     if(n==1):
-        comparador=True   
+        comparador=True  #condicion de frenado del ciclo
     else:
-        cadena=chr(random.randint(33,126))+cadena
+        cadena=chr(random.randint(33,126))+cadena #se le agrega un valor aleatorio a la cadena y se vuelve a empezar
 
